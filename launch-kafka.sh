f390a06ada76dfe4c1ae82bf313ddd648fb5bd01
adnls <david.ayache90@gmail.com>
2020-03-07 21:47:55 +0100

    lauch-kafka.sh added

---
+++ launch-kafka.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+~/kafka/kafka_2.12-2.4.0/bin/zookeeper-server-start.sh ~/kafka/kafka_2.12-2.4.0/config/zookeeper.properties &
+~/kafka/kafka_2.12-2.4.0/bin/kafka-server-start.sh ~/kafka/kafka_2.12-2.4.0/config/server.properties &
